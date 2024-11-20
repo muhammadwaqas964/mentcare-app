4b7069ce54b3857c551430c92aeb57817d975ee8
AkulCoder <106450550+AkulCoder@users.noreply.github.com>
2024-11-20 17:12:59 -0500

    Add files via upload

--- frontend/src/components/TherapistProfile.js
+++ frontend/src/components/TherapistProfile.js
@@ -1,43 +1,42 @@
 import React from 'react';
 import './TherapistProfile.css';
 import profilePicture from './assets/images/img1_th_mike.png';
+import { useNavigate } from 'react-router-dom';
+import './TherapistDashboard.js';
 
 function TherapistProfile() {
+    const navigate = useNavigate();
+    
+    const goToHomePage = () => {
+        navigate('/');
+    };
     return (
         <div className="therapist-profile">
             <header className="header">
                 <h1>MentCare</h1>
-                <button className="login-btn">Login</button>
+                <button className="nav-btn"  onClick={goToHomePage} >Home</button>
             </header>
 
             <section className="profile">
                 <img src={profilePicture} alt="Mike K." className="profile-picture" />
                 <h2>About Me</h2>
                 <p>
-                    Hello, I'm Mike K., a licensed psychotherapist with over 15 years of experience in mental health care. I specialize in cognitive-behavioral therapy (CBT) and mindfulness-based approaches to help individuals navigate challenges such as anxiety, depression, and stress. My goal is to create a supportive and non-judgmental environment where clients feel empowered to explore their thoughts and emotions. Together, we can work towards achieving your personal goals and enhancing your overall well-being.
+                    Hello, I'm Mike Ken, a licensed mentalcare therapist with over 15 years of experience in mental health care. I specialize in cognitive-behavioral therapy (CBT) and mindfulness-based approaches to help individuals navigate challenges such as anxiety, depression, and stress. My goal is to create a supportive and non-judgmental environment where clients feel empowered to explore their thoughts and emotions. Together, we can work towards achieving your personal goals and enhancing your overall well-being.
                 </p>
-                <p><strong>Therapist Name:</strong> Mike K.</p>
+                <p><strong>Therapist Name:</strong> Mike Ken</p>
+                <p><strong>Degree/Certifications:</strong> Master of Science in Clinical Mental Health Counseling, Licensed Professional Counselor </p>
             </section>
 
             <section className="availability">
                 <h3>Availability</h3>
-                <p>Monday - Friday: 8am - 12:30pm & 3pm - 8pm</p>
+                <p>Monday - Friday: 8:00 am - 12:30 pm & 3:00 pm - 8:00 pm</p>
             </section>
 
             <section className="pricing">
                 <h3>Pricing</h3>
                 <p>$15 per hour</p>
-                <button className="reach-out-btn">Reach Out</button>
             </section>
 
-            <section className="reviews">
-                <h3>Reviews</h3>
-                <div className="rating-bar">5★</div>
-                <div className="rating-bar">4★</div>
-                <div className="rating-bar">3★</div>
-                <div className="rating-bar">2★</div>
-                <div className="rating-bar">1★</div>
-            </section>
 
             <section className="patient-reviews">
                 <h3>Patient Reviews</h3>
