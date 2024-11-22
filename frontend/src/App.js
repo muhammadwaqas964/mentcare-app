0052c22b4e216a5df6e120f7fdce09c1ee51c730
AkulCoder <106450550+AkulCoder@users.noreply.github.com>
2024-11-21 19:38:47 -0500

    Add files via upload
    Added Therapist Profile Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import TherapistProfile from './pages/TherapistProfile';
 import Chat from './pages/Chat';
 import SettingsPage from './pages/Settings';
 
@@ -20,6 +21,7 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/therapistprofile' element={<TherapistProfile />} />
         <Route path='/chat' element={<Chat />} />
         <Route path='/settings' element={<SettingsPage />} />
       </Routes >
