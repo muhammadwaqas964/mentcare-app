e4e8eef82c588a87c80593b9ea22e69e90714739
icupo3 <icup.jm@gmail.com>
2024-11-22 16:54:03 -0500

    Update settings.py
    forgot to push this as well

--- backend/endpoints/settings.py
+++ backend/endpoints/settings.py
@@ -87,3 +87,67 @@ def settingsUpdPrivacyFunc():
             return jsonify({"inserted" : 0}), 200
     except Exception as err:
         return {"error":  f"{err}"}
+
+@settingsPageData.route('/settingsRemoveAccount', methods=['POST'])
+def settingsRemAccFunc():
+    try:
+        userId = request.json.get('userId')
+        userType = request.json.get('userType')
+
+        cursor = mysql.connection.cursor()
+
+        if (userType == "Patient"):
+            cursor.execute(f'SELECT invoiceID FROM invoices WHERE invoices.patientID = {userId}')
+            if(cursor.rowcount > 0):
+                cursor.close()
+                return jsonify({"deletion" : "cant delete, patient owes money"}), 200
+
+            cursor.execute(f'SELECT userID FROM patients WHERE patients.patientID = {userId}')
+            realUserId = cursor.fetchone()[0]
+
+            # chats: deps on patients
+            # completedDailySurveys: deps on patients
+            # completedSurveys: deps on patients
+            # feedback: deps on patients
+            # journals: deps on patients
+            # surveys: deps on patients
+            # therapistPatientsList: deps on patients
+            cursor.execute(f'''
+                DELETE FROM chats WHERE patientID = {userId}''')
+            cursor.execute(f'''
+                DELETE FROM completedDailySurveys WHERE patientID = {userId}''')
+            cursor.execute(f'''
+                DELETE FROM completedSurveys WHERE patientID = {userId}''')
+            cursor.execute(f'''
+                DELETE FROM feedback WHERE patientID = {userId}''')
+            cursor.execute(f'''
+                DELETE FROM journals WHERE patientID = {userId}''')
+            cursor.execute(f'''
+                DELETE FROM surveys WHERE patientID = {userId}''')
+            cursor.execute(f'''
+                DELETE FROM therapistPatientsList WHERE patientID = {userId}''')
+            # notifications: deps on users
+            # patients: deps on users
+            # users: no deps
+            cursor.execute(f'''
+                DELETE FROM notifications WHERE userID = {realUserId}''')
+            cursor.execute(f'''
+                DELETE FROM patients WHERE userID = {realUserId}''')
+            cursor.execute(f'''
+                DELETE FROM users WHERE userID = {realUserId}''')
+        elif (userType == "Therapist"):
+            pass
+        else:
+            cursor.close()
+            return jsonify({"deletion" : "cant delete, patient owes money"}), 200
+        
+        mysql.connection.commit()
+        
+        if(cursor.rowcount > 0): # We ensure the table was modified
+            cursor.close()
+            return jsonify({"deleted" : 1}), 200
+        else:
+            cursor.close()
+            return jsonify({"deleted" : 0}), 200
+    except Exception as err:
+        return {"error":  f"{err}"}
