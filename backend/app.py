0d2e3389d9129bb8988f04c5498ef6fc4307e506
fmartinez0530 <alitomartinez0530@gmail.com>
2024-12-14 12:57:04 -0500

    Added handling for OPTIONS

--- backend/app.py
+++ backend/app.py
@@ -68,6 +68,16 @@ app.register_blueprint(TherapistDashboardData)
 from endpoints.settings import settingsPageData
 app.register_blueprint(settingsPageData)
 
+@app.before_request
+def handle_options():
+    if request.method == "OPTIONS":
+        response = app.make_response()
+        response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        return response, 200
+
 @app.after_request
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
