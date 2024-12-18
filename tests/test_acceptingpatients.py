c54cf386250ec00af9184ab59f4765881c916652
Muhammad Waqas <124439937+muhammadwaqas964@users.noreply.github.com>
2024-12-19 02:19:46 +0500

    Update test_acceptingpatients.py

--- tests/test_acceptingpatients.py
+++ tests/test_acceptingpatients.py
@@ -14,7 +14,7 @@ def get_driver():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = "/usr/bin/google-chrome"  # Make sure this path is correct
+    chrome_options.binary_location = "/usr/bin/google-chrome"  # Ensure this path is correct
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
